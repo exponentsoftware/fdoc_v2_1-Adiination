38f352b61e6110d59a61ec50b63326280979e227
Adiination <Aditya.sasaram11@gmail.com>
2023-05-14 16:11:09 +0530

    code solution of question 1b

---
+++ 1b.js
@@ -0,0 +1,15 @@
+
+// 1.b. Write a function called filterByLength that takes an array of strings and a 
+// number as input and returns an array of all the strings in the input array 
+// whose length is greater than or equal to the input number.
+
+function filterByLength(arr, num) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].length >= num) {
+        result.push(arr[i]);
+      }
+    }
+    return result;
+  }
+  
