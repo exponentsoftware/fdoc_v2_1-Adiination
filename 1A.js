fe2b15c1f47be69094d2635b900f3a3ec7d237f2
Adiination <Aditya.sasaram11@gmail.com>
2023-05-14 16:05:31 +0530

    new answer file

--- 1A.js
+++ 1A.js
@@ -1,7 +1,7 @@
 // 1.a. Write a function called countOccurrences that takes an array of numbers and a 
 // number as input and returns the number of times the input number occurs in the array.
 
-function count(array,Num){
+function countOccurrences(array,Num){
     var count = 0;
     for(var i = 0; i < array.length; i++){
         if(array[i]==Num){
@@ -13,4 +13,4 @@ return count;
 }
 
 
-console.log(count([2,3,4,2,3,5,5,2],4));
+console.log(countOccurrences([2,3,4,2,3,5,5,2],5));
