1cd096860d83e95acd24f70596f9771873c52dc7
Adiination <Aditya.sasaram11@gmail.com>
2023-05-16 19:11:26 +0530

    new answer code

---
+++ 2c.js
@@ -0,0 +1,3 @@
+//2c.Write a function called `findNthLargest` that takes an array of numbers and a number n 
+//as input and returns the nth largest number in the array.
+
---
+++ 2d.js
@@ -0,0 +1,3 @@
+// 2.d. Write a function called `findMaximumSubarray` that takes an array of integers 
+// as input and returns the maximum sum of any contiguous subarray within the input array.
+
