5bbfd24ca24fef75b901e5291912ec05a4941094
Adiination <Aditya.sasaram11@gmail.com>
2023-05-18 19:51:29 +0530

    answer of 2d

---
+++ 1d.js
@@ -0,0 +1,6 @@
+// 1.d. Write a function called `longestIncreasingSubsequence` that takes an array
+//  of numbers as input and returns the length of the longest increasing subsequence in the array.
+//   A subsequence is a sequence that can be derived from another sequence
+//  by deleting some or no elements without changing the order of the remaining elements.
+
+
--- 2d.js
+++ 2d.js
@@ -1,3 +1,24 @@
 // 2.d. Write a function called `findMaximumSubarray` that takes an array of integers 
 // as input and returns the maximum sum of any contiguous subarray within the input array.
 
+function findMaximumSubarray(nums) {
+    if (nums.length === 0) {
+      return 0;
+    }
+  
+    let maxSum = nums[0];
+    let currentSum = nums[0];
+  
+    for (let i = 1; i < nums.length; i++) {
+      currentSum = Math.max(nums[i], currentSum + nums[i]);
+      maxSum = Math.max(maxSum, currentSum);
+    }
+  
+    return maxSum;
+  }
+  
+  const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+  console.log(findMaximumSubarray(arr));
+  
+
+
