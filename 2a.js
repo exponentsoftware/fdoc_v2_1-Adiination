74a754d6debaf1da5449495fd9906b26452a15ed
Adiination <Aditya.sasaram11@gmail.com>
2023-05-18 16:31:34 +0530

    2a solution

--- 2a.js
+++ 2a.js
@@ -7,9 +7,9 @@
 function findPairs(arr, target) {
     const pairs = [];
   
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = i + 1; j < arr.length; j++) {
-        if (arr[i] + arr[j] === target) {
+    for (let i = 0; i < arr.length; i++) { //looping through the array from the index 0 
+      for (let j = i + 1; j < arr.length; j++) { //looping through the array again by leaving the first number 
+        if (arr[i] + arr[j] === target) { //if their sum is equal to the target ,it will push to the pairs variable
           pairs.push([arr[i], arr[j]]);
         }
       }
