9f6169083b939988f8bcea4e22155eed88437ffb
Adiination <Aditya.sasaram11@gmail.com>
2023-05-19 15:42:24 +0530

    answer code of 2c

--- 2c.js
+++ 2c.js
@@ -1,3 +1,10 @@
 //2c.Write a function called `findNthLargest` that takes an array of numbers and a number n 
 //as input and returns the nth largest number in the array.
 
+function findNthLargest(array,number){
+    let newArr = array.sort((a,b) => b-a);
+    return newArr[number-1];
+}
+
+let a = [2,3,5,6,7,3];
+console.log(findNthLargest(a,2));
